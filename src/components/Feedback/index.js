c91e516c07790006c7401921d37181284d56d333
sai kumar <saikumarb8100@gmail.com>
2024-06-14 10:30:11 +0000

    Total Feedback App File

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -5,19 +5,51 @@ import './index.css'
 import FeedBackItem from '../FeedbackItem'
 
 class Feedback extends Component {
-  render() {
-    const {resources} = this.props
-    const {emojis} = resources
+  constructor(props) {
+    super(props)
+    this.state = {
+      resourcesList: props.resources,
+      isHappy: false,
+    }
+  }
 
+  happyEmojiStatus = () => {
+    this.setState({isHappy: true})
+  }
+
+  render() {
+    const {resourcesList, isHappy} = this.state
+    const {emojis, loveEmojiUrl} = resourcesList
+
+    if (isHappy === false) {
+      return (
+        <div className="main-container">
+          <div className="feedback-container">
+            <h1>How satisfied are you with our customer support performance</h1>
+            <ul>
+              {emojis.map(eachObj => (
+                <FeedBackItem
+                  emojis={eachObj}
+                  key={eachObj.id}
+                  happyEmojiStatus={this.happyEmojiStatus}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      )
+    }
     return (
       <div className="main-container">
         <div className="feedback-container">
-          <h1>How satisfied are you with our customer support performance</h1>
-          <ul>
-            {emojis.map(eachObj => (
-              <FeedBackItem emojis={eachObj} key={eachObj.id} />
-            ))}
-          </ul>
+          <div className="love-emoji-container">
+            <img src={loveEmojiUrl} alt="love emoji" className="love-symbol" />
+            <h1 className="thank-para">Thank You</h1>
+          </div>
+          <p className="support-text-para">
+            We will use your feedback to improve out customer support
+            performance
+          </p>
         </div>
       </div>
     )
--- src/components/FeedbackItem/index.js
+++ src/components/FeedbackItem/index.js
@@ -1,11 +1,14 @@
 import './index.css'
 
 const FeedBackItem = props => {
-  const {emojis} = props
-  const {imageUrl, name} = emojis
+  const {emojis, happyEmojiStatus} = props
+  const {imageUrl, name, id} = emojis
+  const happyEmoji = () => {
+    happyEmojiStatus(id)
+  }
   return (
     <li>
-      <img src={imageUrl} alt={name} />
+      <img src={imageUrl} alt={name} onClick={happyEmoji} />
       <p>{name}</p>
     </li>
   )
